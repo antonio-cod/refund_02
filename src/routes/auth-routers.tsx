5b81131484618212a361ef1377a50b5b85839af0
antonio-cod <antonio_carlostads@hotmail.com>
2025-06-14 07:20:46 -0400

    Feat: creating input component

--- src/routes/auth-routers.tsx
+++ src/routes/auth-routers.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router";
-import { SignIn } from "../page/SignIn";
+import { SignIn } from "../pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
 
 
@@ -7,8 +7,9 @@ import { AuthLayout } from "../components/AuthLayout";
 export function AuthRoutes(){
   return (
     <Routes>
-        <Route path="/" element={<AuthLayout/>} />
-      <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="/" element={<SignIn />} />
+        </Route> 
     </Routes>
   )
 }

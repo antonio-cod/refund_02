be0a94c9023d3575ccd1822b13838fca5654c871
antonio-cod <antonio_carlostads@hotmail.com>
2025-06-19 22:57:01 -0400

    Feat: Creating the Dashboard Page

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,13 +1,28 @@
 import { Navigate, useLocation } from "react-router"
+import okSvg from "../assets/ok.svg"
 
 
-export function Confirm (){
+export function Confirm() {
   const location = useLocation()
 
-  if(!location.state?.fromSubmit){
+  if (!location.state?.fromSubmit) {
     return <Navigate to="/" />
   }
-  return(
-    <h1>Confirm</h1>
+  return (
+    <div className="bg-gray-500 lg:w-[512px] rounded-xl flex
+     flex-col items-center p-10 gap-6">
+      <h1 className="text-2xl font-bold text-center text-green-100">
+        Solicitação enviada!
+      </h1>
+      <img src={okSvg} alt="Ícone de OK" className="w-28" />
+
+      <p>
+        Agora é apenas aguardar! sua solicitação sera analisada pelo
+        departamento, em breve o setor entrara em contato!
+      </p>
+
+      <a href="/" className="w-full p-3 text-center bg-green-200 rounded-lg
+       text-white transition ease-linear">Nova solicitação</a>
+    </div>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router";
-import { AuthRoutes } from "./authRouters";
-import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      {/* <AuthRoutes /> */}
-      <EmployeeRoutes />
+      <ManagerRoutes />
     </BrowserRouter>
 
   )

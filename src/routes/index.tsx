32cf8515f98a5339ed479acbccb9bc2100d5e3fa
antonio-cod <antonio_carlostads@hotmail.com>
2025-06-24 19:25:32 -0400

    Feat: Loading Component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { BrowserRouter } from "react-router";
 import { ManagerRoutes } from "./ManagerRoutes";
+import { AuthRoutes } from "./authRouters";
+import { Loading } from "../components/Loading";
+
+const isLoading = true
 
 export function Routes() {
+  if (isLoading) {
+    return <Loading />
+  }
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      {/* <ManagerRoutes /> */}
+      <AuthRoutes />
     </BrowserRouter>
 
   )

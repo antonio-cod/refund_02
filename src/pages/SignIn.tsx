a99c56d2a070dacbf1a9315690cf88e5feb223db
antonio-cod <antonio_carlostads@hotmail.com>
2025-06-24 22:04:45 -0400

    Feat: Data validation using Zod

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,55 +1,71 @@
 import { useActionState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { z, ZodError } from "zod";
 
+const signInSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z.string().trim().min(1, { message: "Informe a senha" }),
+})
 
 export function SignIn() {
   const [state, formAction, isLoading] = useActionState(signIn, null)
 
-    async function signIn(prevState: any, formData: FormData) {
-      const email = formData.get("email")
-      const password = formData.get("password")
-
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve("ok")
-        }, 3000)
+  async function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
       })
 
-    
-    
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof ZodError) {
+        return {message: error.issues[0].message}
+      }
+
+      return { message: "Não foi possivel entrar!" }
+    }
   }
+
+
   return (
     <form action={formAction} className="w-full flex flex-col gap-4">
-      <Input  
-      name="email"
-        required 
+      <Input
+        name="email"
+        required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        
-        
+
+
       />
 
-      <Input  
-      name="password"
-        required 
+      <Input
+        name="password"
+        required
         legend="Senha"
         type="password"
         placeholder="123456"
-      
+
       />
+
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.message}
+      </p>
 
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
 
-      <a 
-       href="/signup" 
-       className="text-sm font-semibold text-gray-100 mt-10
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10
         mb-4 text-center hover:text-green-800 transition ease-linear"
       >
-       Criar conta
+        Criar conta
       </a>
     </form>
   )
